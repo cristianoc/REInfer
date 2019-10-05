68bd6c3484744b8b0f5d2011daded2329c113228
Cristiano Calcagno <ccrisccris@gmail.com>
2019-10-05 03:25:11 +0200

    uglify rollup

--- rollup.config.browser.js
+++ rollup.config.browser.js
@@ -1,32 +1,30 @@
 import resolve from "rollup-plugin-node-resolve";
 import commonjs from "rollup-plugin-commonjs";
 import replace from "rollup-plugin-replace";
+import { uglify } from "rollup-plugin-uglify";
 
 export default {
-  input: "src/Index.js",
-  output: {
-    file: "bundledOutputs/rollup.js",
-    format: "umd",
-    name: "main"
-  },
-  plugins: [
-    resolve(),
-    commonjs({
-      include: "node_modules/**",
-      namedExports: {
-        // left-hand side can be an absolute path, a path
-        // relative to the current directory, or the name
-        // of a module in node_modules
-        "react-dom": ["render", "hydrate"],
-        react: ["createElement", "useReducer"]
-      }
-    }),
-    replace({
-      "process.env.NODE_ENV": "'production'"
-    })
-  ],
-  //  external: ["react", "react-dom"],
-  globals: {
-    //    react: "React"
-  }
+  input: "src/Index.js",
+  output: {
+    file: "bundledOutputs/rollup.js",
+    format: "umd",
+    name: "main"
+  },
+  plugins: [
+    resolve(),
+    commonjs({
+      include: "node_modules/**",
+      namedExports: {
+        // left-hand side can be an absolute path, a path
+        // relative to the current directory, or the name
+        // of a module in node_modules
+        "react-dom": ["render", "hydrate"],
+        react: ["createElement", "useReducer"]
+      }
+    }),
+    replace({
+      "process.env.NODE_ENV": "'production'"
+    }),
+    uglify()
+  ]
 };
